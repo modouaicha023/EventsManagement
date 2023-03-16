2b435abd41c6e1627fdff52aff6095ff38665b8d
Mnafyndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2023-03-16 20:32:55 +0000

    Update App.java

--- App.java
+++ App.java
@@ -14,11 +14,17 @@ public class App {
         System.out.println("| ✦ Le meilleur dans l'organisation d'évènement✦    |");
         System.out.println("|                                                    |");
         System.out.println("╰────────────────────────────────────────────────────╯");
-        System.out.println("         Veuillez faire un choix                    ");
-        System.out.println(" 1 ❀  recherche de salles  ");
-        System.out.println(" 2 ❀ détails de la salle [numéro] ");
-        System.out.println(" 3 ❀ Quitter (x_x)");
-        System.out.println("Veuillez choisir parmi ces options : ");
+        System.out.println(" 1 ❀        rechercher les salles                   ❀");
+        System.out.println(" 2 ❀            Se Connecter                        ❀");
+        System.out.println(" 3 ❀             S'inscrire                         ❀");
+        System.out.println(" 4 ❀               Quitter                          ❀");
+        System.out.println("         Veuillez choisir parmi ces options :         ");
+        System.out.println("         ────────────────────────────────────         ");
+        System.out.println("                    \\   ^__^                         ");
+        System.out.println("                     \\  (oo)\\ _______               ");
+        System.out.println("                      \\ (__)\\        )\\/\\         ");
+        System.out.println("                             ||----w |                ");
+        System.out.println("                             ||     ||                ");
         int choix = sc.nextInt();
         return choix;
     }
@@ -33,5 +39,17 @@ public class App {
         System.out.println("Veuillez choisir parmi ces options : ");
         int choix1 = sc.nextInt();
         return choix1;
+    }
+
+    public static void Sinscrire() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("✦veuiller entrer votre Nom   ✦");
+        varchar nom = sc.next();
+        System.out.println("✦veuiller entrer votre Prenom✦");
+        varchar prenom = sc.next();
+        system.out.println("✦veuiller entrer votre Email    ✦");
+        varchar email = sc.next();
+        system.out.println("✦veuiller entrer votre Adresse  ✦");
+        varchar adresse = sc.next();
     }
 }
