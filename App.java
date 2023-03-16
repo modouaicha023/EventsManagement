0db02a962c5f3e7a2265a170a9c2033cb70f831c
Mnafyndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2023-03-16 19:38:13 +0000

    commit 2

--- App.java
+++ App.java
@@ -8,11 +8,16 @@ public class App {
 
     public static int acceuilPrincipale() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n-------- Bienvenue Chez M2A_Events :) --------");
-        System.out.println("Avec Nous Votre Evenement Sera un Succes \\(°o°)/\n");
-        System.out.println(" 1 --> Visualiser les Salles  ('_')");
-        System.out.println(" 2 --> Visualiser les Décorations ");
-        System.out.println(" 3 --> Quitter (x_x)");
+        System.out.println("╭────────────────────────────────────────────────────╮");
+        System.out.println("|                                                    |");
+        System.out.println("|           ✦ Bienvenue Chez M2A_Events ✦           |");
+        System.out.println("| ✦ Le meilleur dans l'organisation d'évènement✦    |");
+        System.out.println("|                                                    |");
+        System.out.println("╰────────────────────────────────────────────────────╯");
+        System.out.println("         Veuillez faire un choix                    ");
+        System.out.println(" 1 ❀  recherche de salles  ");
+        System.out.println(" 2 ❀ détails de la salle [numéro] ");
+        System.out.println(" 3 ❀ Quitter (x_x)");
         System.out.println("Veuillez choisir parmi ces options : ");
         int choix = sc.nextInt();
         return choix;
@@ -20,13 +25,13 @@ public class App {
 
     public static int visualiserSalle(int choix) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n-------- Voici les Salles Disponible dans Notre Compagnie :) --------");
+        System.out.println(".·:*¨༺ ༻¨*:·. Voici les Salles Disponible dans Notre Compagnie .·:*¨༺ ༻¨*:·.");
         System.out.println("\n");
-        System.out.println(" 1 --> Visualiser les Salles  ('_')");
+        System.out.println(" 1 --> Visualiser les Salles  🌷");
         System.out.println(" 2 --> Visualiser les Décorations ");
         System.out.println(" 3 --> Quitter (x_x)");
         System.out.println("Veuillez choisir parmi ces options : ");
         int choix1 = sc.nextInt();
-        return choix;
+        return choix1;
     }
 }
--- Reservation.java
+++ Reservation.java
@@ -10,33 +10,57 @@ public class Reservation {
     private int idSalle;
     private LocalTime dureReservation;
     private LocalDate dateReservation;
-    
-    
 
-    public Reservation(int idReservation, int idClient, int idSalle, LocalTime dureReservation, LocalDate dateReservation) {
-        this.idReservation=idReservation;
-        this.idClient=idClient;
+    public Reservation(int idReservation, int idClient, int idSalle, LocalTime dureReservation,
+            LocalDate dateReservation) {
+        this.idReservation = idReservation;
+        this.idClient = idClient;
         this.idSalle = idSalle;
         this.dateReservation = dateReservation;
-        this.dureReservation=dureReservation;
+        this.dureReservation = dureReservation;
     }
-    public Reservation(){};
 
-    public int getIdReservation() {return idReservation;}
-    public int getIdClient() {return idClient;}
-    public int getIdSalle() {return idSalle;}
-    public LocalDate getDateReservation() {return dateReservation;}
-    public LocalTime getDureReservation() {return dureReservation;}
+    public Reservation() {
+    };
 
+    public int getIdReservation() {
+        return idReservation;
+    }
 
-    public void setidReservation(int idReservation){this.idReservation=idReservation;}
-    public void setIdSalle(int idSalle) {this.idSalle=idSalle;}
-    public void setidClient(int idClient) {this.idClient=idClient;}
-    public void setdateReservation(LocalDate dateReservation){this.dateReservation=dateReservation;}
-    public void setDureReservation(LocalTime dureReservation){this.dureReservation=dureReservation;}
+    public int getIdClient() {
+        return idClient;
+    }
 
+    public int getIdSalle() {
+        return idSalle;
+    }
 
+    public LocalDate getDateReservation() {
+        return dateReservation;
+    }
 
-    
+    public LocalTime getDureReservation() {
+        return dureReservation;
+    }
+
+    public void setidReservation(int idReservation) {
+        this.idReservation = idReservation;
+    }
+
+    public void setIdSalle(int idSalle) {
+        this.idSalle = idSalle;
+    }
+
+    public void setidClient(int idClient) {
+        this.idClient = idClient;
+    }
+
+    public void setdateReservation(LocalDate dateReservation) {
+        this.dateReservation = dateReservation;
+    }
+
+    public void setDureReservation(LocalTime dureReservation) {
+        this.dureReservation = dureReservation;
+    }
 
 }
